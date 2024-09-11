234df8800c9334e96e816401d6bb5fb707511bce
Gwyneth Garramone <ggarram1@jh.edu>
2024-09-11 16:20:41 -0400

    Updated afternoon assignment

--- d4/d4_afternoonassignment.R
+++ d4/d4_afternoonassignment.R
@@ -29,22 +29,29 @@ dnm_by_parental_age <- left_join(dnm_summary, ages, by = "Proband_id")
 
 #2.2:
 #full_model <- lm(data=dnm_by_parental_age,
-                 #formula = n_maternal_dnm ~1 + Mother_age)
+ #                formula = n_maternal_dnm ~1 + Mother_age)
 #summary(full_model)
 
 #the "size" of the relationship can be determined based on the slope(maternal), at 0.378, this is 
 #a positive slope. This means that as the mother gets older, there is a small increase
 #in number of dnms. However, the Adjusted R-squared(maternal) is 0.23, this shows that this 
 #is a mildly strong relationship.
+#The "significance" of this relationship is denoted by the p-value (<2.2e^-16)
+#This is a very low p-value which shows that the relationship is not due to chance,
+#but, instead, is statistically significant.
 
 #2.3
-full_model <- lm(data=dnm_by_parental_age,
-                 formula = n_paternal_dnm ~1 + Father_age)
-summary(full_model)
+#full_model <- lm(data=dnm_by_parental_age,
+ #               formula = n_paternal_dnm ~1 + Father_age)
+#summary(full_model)
 #the "size" of the relationship can be determined based on the slope(paternal), at 1.35, this is 
 #a positive slope. This means that as the father gets older, there is an increase
 #in number of dnms. However, the Adjusted R-squared(paternal) is 0.62, this shows that this 
 #is a stronger relationship than observed between maternal age and maternal dnms.
+#The "significance" of this relationship is denoted by the p-value (<2e^-16)
+#This is a very low p-value which shows that the relationship is not due to chance,
+#but, instead, is statistically significant.This value is slightly higher than that of
+#the maternal data. 
 
 #2.4
 #y=b0 + b1*x1 + b2*x2
@@ -69,9 +76,17 @@ t.test(dnm_by_parental_age$n_maternal_dnm, dnm_by_parental_age$n_paternal_dnm)
 summary(plot)
 
 #An (unpaired) T test was chosen as the maternal and paternal DNM counts are
-#independent from one another.
+#separate measurements, rather than repeated measurements of the same variable.
+#This test is to consider these separately - that is, we are looking to determine 
+#if there is a difference in number of paternal DNMs depending on father age
+#and the number of maternal DNMs depending on mother age - we are not asking if
+#the number of Maternal DNMs at a given age is different to the number of paternal
+#DNMs at the same age/in the same organism.
+#This uses, therefore, Welch's two sample t-test
 #The p value from the t test is 2.2e^-16. This suggest a non-random cause
 #for the difference. 
+#This test is 2-tailed to consider both the positive and negative tails of the distribution.
+
 
 
 #close the file
