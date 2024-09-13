02b69eb42a8f27693c76f2efa98040f7761ee927
Gwyneth Garramone <ggarram1@jh.edu>
2024-09-13 18:30:52 -0400

    Updated Lunch Assignment

--- d4/d4_LunchAssignment.py
+++ d4/d4_LunchAssignment.py
@@ -17,12 +17,14 @@ for line in fs:
     key = (fields[0], fields[2])
 #create new dictionary from key with list to hold samples
     rel_samples[key] = []
-#print(rel_samples) - to check if this part has worked
+#print(rel_samples) 
+
 # - to check before continuing the code
+
 #2:determine which tissue scorresponds to which sample_ID
 #create key from gene and tissue^ 
 
-#get metadata file name:
+#get sample_IDs file:
 filename = sys.argv[2]
 #open file in reading mode
 fs=open(filename, mode='r')
@@ -35,7 +37,7 @@ for line in fs:
 #split line into fields and remove next line characters
     fields=line.rstrip("\n").split("\t")
 #assign key to gene and value to tissue
-    key = fields[6] 
+    key = fields[2] 
     value =fields[0]
 #initialize dict from key with list to hold samples
 #append the value (column) after the tissue sample 
@@ -44,7 +46,8 @@ for line in fs:
 #close the file
 #fs.close()
 
-#print(tissue_samples) - to check before continuing the code
+#print(tissue_samples)
+# - to check before continuing the code
 
 #3:determine sample_IDs present in gene expression file
 #get expression data file:
@@ -73,31 +76,26 @@ for tissue, samples in tissue_samples.items():
             tissue_columns[tissue].append(position)
 #print(tissue_columns)
 
-for key, value in tissue_columns.items():
-#   print(key, len(value))
+#for key, value in tissue_columns.items():
+#    print(key, len(value))
 
-    max_value = 0
-    max_tissue = ""
 
-for tissue, samples in tissue_columns.item():
+max_value = 0
+max_tissue = ""
+
+for tissue, samples in tissue_columns.items():
     if len(samples)>= max_value:
         max_value = len(samples)
         max_tissue = tissue
 
-print (max_tissue, max_value)
+print(max_tissue, max_value)
 #organs with greater tissue diversity, ex. brain, have the highest nunber of samples.
 #smaller organs, ex. spleen, have the fewest samples.
-#During a git push problem, I had had to delete a couple of the GTEx files (I accidentally commited them)
-#So I cannot re-run the code anymore (without re-downloading but I am not sure what the source is)
-#This is why I have not included numbers above.
 
 
 
-# #close the file
+#close the file
 #fs.close() 
-#for key, value in tissue_columns.items():
-
-#    print(key, value)
 
 #question 7 on other doc
 
